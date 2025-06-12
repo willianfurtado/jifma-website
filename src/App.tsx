82816bc44a8f5177f872d27a8108bf4dbce3a0cb
willianfurtado <willianjj15@hotmail.com>
2025-06-11 21:55:17 -0300

    Responsividade da página de Notícias

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
       <Header />
       <main className="max-w-[90%] mx-auto">
         <h1 className="text-2xl font-bold text-black my-4">Notícias</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           <News />
           <News />
           <News />
--- src/Components/News.tsx
+++ src/Components/News.tsx
@@ -1,7 +1,7 @@
 export default function News() {
   return (
     <>
-      <div className="w-70 h-50 bg-zinc-400 rounded-2xl"></div>
+      <div className="w-full h-[200px] bg-zinc-400 rounded-2xl"></div>
     </>
   );
 }
