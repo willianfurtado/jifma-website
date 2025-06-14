9877216b0d1eac57bc0141a7132e8ab52f91ef68
willianfurtado <willianjj15@hotmail.com>
2025-06-14 11:15:08 -0300

    Ajustes visuais no card de partidas

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -16,16 +16,16 @@ interface iProps {
 export default function Card(props: iProps) {
   return (
     <div className="h-full w-full border-2 border-green-800 rounded-xl shadow-md">
-      <div className="p-4 flex flex-col items-center justify-center h-full">
-        <h1 className="uppercase text-xl font-semibold text-green-800 text-center">
+      <div className="p-3.5 flex flex-col items-center justify-center h-full">
+        <h1 className="uppercase text-2xl font-semibold text-green-800 text-center">
           {props.modalidade}
         </h1>
 
         <hr className="border-t border-green-800 my-3 w-[90%]" />
 
-        <div className="flex items-center justify-center gap-4">
+        <div className="flex items-center justify-center gap-7 md:gap-5">
           {/* Data e bola */}
-          <div className="flex flex-col items-center gap-1">
+          <div className="flex flex-col items-center justify-center gap-3">
             <img src={ball} className="h-14 w-14" />
             <div className="text-center">
               <p className="text-2xl font-bold text-green-800">{props.data}</p>
@@ -34,21 +34,21 @@ export default function Card(props: iProps) {
           </div>
 
           {/* Informações da partida */}
-          <div className="flex flex-col gap-2 text-center text-sm">
-            <div className="flex items-center justify-center gap-2">
-              <img src={clock} className="h-5 w-5" />
+          <div className="flex flex-col gap-3 justify-center text-lg md:text-md lg:text-lg w-[70%]">
+            <div className="flex items-center gap-2">
+              <img src={clock} className="h-6 w-6" />
               <span>
                 {props.equipe1} <strong className="text-green-800">x</strong> {props.equipe2}
               </span>
             </div>
 
-            <div className="flex items-center justify-center gap-2">
-              <img src={calendar} className="h-5 w-5" />
+            <div className="flex items-center  gap-2">
+              <img src={calendar} className="h-6 w-6" />
               <span>{props.horario}</span>
             </div>
 
-            <div className="flex items-center justify-center gap-2">
-              <img src={place} className="h-5 w-5" />
+            <div className="flex items-center gap-2">
+              <img src={place} className="h-6 w-6" />
               <span>{props.local}</span>
             </div>
           </div>
